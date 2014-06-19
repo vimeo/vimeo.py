c5ca02577b1cafba801f49d4776f9f18bdbae4d0
Joe Cabrera <calcmaster16@gmail.com>
2014-06-19 16:46:09 +0000

    Revert "moved testing dev data to seperate file"
    This reverts commit 7e894c872a3c73596dfd0fb99704f532d88b3ee5.

--- test/test_vimeo.py
+++ test/test_vimeo.py
@@ -20,13 +20,30 @@ import pytest
 import os,sys
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0,parentdir)
-import password
 
 from vimeo import VimeoClient
 
 
 vimeo = None
 test_data = {}
+
+dev_data = {
+    'user': "12233214",
+    'access_token': "58b68afc670f2b3bf8836e56f4879af8",  # fully privileged access token belonging to testuser
+    'client_id': "90bed05a315cf49d45ca2e453179eadb81e62ed7",
+    'client_secret': "bd91cfd28bbc0fc747189528e950df7a1c8b95bb",
+    'channel': "c630249",  # channel owned by testuser
+    'group': "g211471",  # group *NOT* owned by testuser
+    'mygroup': "g206223",  # group *NOT* owned by testuser
+    'presets': "354246",  # presets on video owned by testuser
+    'likevideo': "76115773",  # video not owned by testuser
+    'followuser': "12596222",  # other user
+    'editvideo': "v70429873",  # video owned by testuser
+    'portfolio': "p191937",  # portfolio named "test-portfolio" owned by testuser containing editvideo
+    'album': "a2618551",  # album owned by testuser
+    'category': "comedy",
+    'uploadvideo': "/home/emmett/Desktop/test2.mp4"
+}
 
 prod_data = {
     'user': '20652831',
@@ -41,15 +58,15 @@ prod_data = {
     'uploadvideo': 'testupload.mp4'
 }
 
-for key in password.dev_data:
+for key in dev_data:
     if key not in prod_data:
-        prod_data[key] = password.dev_data[key]
+        prod_data[key] = dev_data[key]
 
 @pytest.fixture
 def setup(dev):
     global vimeo
     global test_data
-    test_data = password.dev_data if dev else prod_data
+    test_data = dev_data if dev else prod_data
     vimeo = VimeoClient(test_data['access_token'], dev=bool(dev))
 
 
