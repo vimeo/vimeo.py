34a437d6df2e48d31249cfc5b64982a6acc59a1e
Stephen McMillen <stephenmc@vimeo.com>
2014-09-23 11:16:21 -0400

    Fix version string building in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 import vimeo
 
 setup(name='PyVimeo',
-    version='.'.join(vimeo.version),
+    version='.'.join(str(v) for v in vimeo.version),
     description='Simple interaction with the Vimeo API.',
     author='Vimeo',
     author_email='support@vimeo.com',
