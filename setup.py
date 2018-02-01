b4def6adfdfdad612041b6247de7083d2af91adf
Jon Ursenbach <jonu@vimeo.com>
2018-02-01 17:44:03 -0500

    Updating the setup version to 1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='PyVimeo',
-    version='0.4.1',
+    version='1.0.0',
     description='Simple interaction with the Vimeo API.',
     url='https://developer.vimeo.com/',
     author='Vimeo',
