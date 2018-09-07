8d20782bf6cf04d604f0f2767bdb6549d9e3e170
Dominick Modica <dmodica2@pride.hofstra.edu>
2018-09-07 11:29:57 -0400

    Specify tuspy version explicitly. Python 3 friendly, upload example.

--- example/upload.py
+++ example/upload.py
@@ -21,7 +21,7 @@ client = vimeo.VimeoClient(
 # Create a variable with a hard coded path to your file system
 file_name = '<full path to a video on the filesystem>'
 
-print 'Uploading: %s' % file_name
+print('Uploading: %s' % file_name)
 
 try:
     # Upload the file and include the video title and description.
@@ -33,7 +33,7 @@ try:
 
     # Get the metadata response from the upload and log out the Vimeo.com url
     video_data = client.get(uri + '?fields=link').json()
-    print '"%s" has been uploaded to %s' % (file_name, video_data['link'])
+    print('"%s" has been uploaded to %s' % (file_name, video_data['link']))
 
     # Make an API call to edit the title and description of the video.
     client.patch(uri, data={
@@ -42,16 +42,16 @@ try:
                        "Python SDK."
     })
 
-    print 'The title and description for %s has been edited.' % uri
+    print('The title and description for %s has been edited.' % uri)
 
     # Make an API call to see if the video is finished transcoding.
     video_data = client.get(uri + '?fields=transcode.status').json()
-    print 'The transcode status for %s is: %s' % (
+    print('The transcode status for %s is: %s' % (
         uri,
         video_data['transcode']['status']
-    )
+    ))
 except vimeo.exceptions.VideoUploadFailure as e:
     # We may have had an error. We can't resolve it here necessarily, so
     # report it to the user.
-    print 'Error uploading %s' % file_name
-    print 'Server reported: %s' % e.message
+    print('Error uploading %s' % file_name)
+    print('Server reported: %s' % e.message)
