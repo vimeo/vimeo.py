7de0bfa7c989d21da6ca85075f0d589a88cde024
Jon Ursenbach <jonu@vimeo.com>
2018-12-05 10:14:59 -0500

    Updating setup.py wth some more information and a stable status.

--- setup.py
+++ setup.py
@@ -6,21 +6,26 @@ try:
 except ImportError:
     from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='PyVimeo',
     version='1.0.9',
     description='Simple interaction with the Vimeo API.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://developer.vimeo.com/',
     author='Vimeo',
     author_email='support@vimeo.com',
-    license='Apache License, Version 2.0, January 2004',
     packages=['vimeo', 'vimeo/auth'],
     install_requires=['requests>=2.4.0', 'tuspy==0.2.1'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: End Users/Desktop',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet',
